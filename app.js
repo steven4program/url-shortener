127e3ec3117fe48b5c179ba51f15c5fb3237a285
steven4program <stevenchang421@gmail.com>
2021-07-31 15:31:34 +0800

    feat: create basic layout

--- app.js
+++ app.js
@@ -2,11 +2,39 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
+const mongoose = require('mongoose')
+
 const app = express()
 const port = 3000
 
+mongoose.connect('mongodb://localhost/url-shortener', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use(methodOverride('_method'))
+
 app.get('/', (req, res) => {
-  res.send('This is url-shortener')
+  res.render('index')
+})
+
+app.post('/shortUrls', (req, res) => {
+  res.render()
 })
 
 app.listen(3000, () => {
