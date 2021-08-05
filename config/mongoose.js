b25c9f862187919b489505252216104fbf900f9e
steven4program <stevenchang421@gmail.com>
2021-08-05 22:38:54 +0800

    chore: heroku init

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 
-const MONGODB_URI = 'mongodb://localhost/url-shortener'
+const MONGODB_URI =
+  process.env.MONGODB_URI || 'mongodb://localhost/url-shortener'
 
 mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
